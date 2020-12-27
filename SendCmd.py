59bc5593e17c77b6ddbfbcb96b0ef7af65655bdc
MoeTheCoder <41845219+MoeTheCoder@users.noreply.github.com>
2020-12-27 12:39:27 -0700

    Update SendCmd.py

--- SendCmd.py
+++ SendCmd.py
@@ -8,7 +8,8 @@ headers = {'Content-Type': 'application/json'}
 deviceID = get_config_value(farmware_name='SendCmd', config_name='deviceID', value_type=str)
 cmd = get_config_value(farmware_name='SendCmd', config_name='cmd', value_type=str)
 param1 = get_config_value(farmware_name='SendCmd', config_name='param1', value_type=int)
-payload = {"DeviceName":deviceID, "CmdName":cmd, "Parameter1": param1}
+param2 = get_config_value(farmware_name='SendCmd', config_name='param2', value_type=int)
+payload = {"DeviceName":deviceID, "CmdName":cmd, "Parameter1": param1, "Parameter2": param2}
 
 resp = requests.post('https://hydrobotapi.azurewebsites.net/api/Hydrobot', json=payload, headers=headers)
 print(resp.text)
