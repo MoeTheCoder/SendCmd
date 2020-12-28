b30bd92ac4ed4c19b3ee0cb8d57e82125a64dc16
MoeTheCoder <41845219+MoeTheCoder@users.noreply.github.com>
2020-12-27 19:33:37 -0700

    Update SendCmd.py

--- SendCmd.py
+++ SendCmd.py
@@ -2,20 +2,14 @@ import requests
 
 # Farmware
 from farmware_tools import get_config_value
-from farmware_tools import device
 
 # Declare vars
 headers = {'Content-Type': 'application/json'}
-device.log('Got the header')
 deviceID = get_config_value(farmware_name='SendCmd', config_name='deviceID', value_type=str)
-device.log('Got the device ID')
 cmd = get_config_value(farmware_name='SendCmd', config_name='cmd', value_type=str)
-device.log('Got the cmd')
-#param1 = get_config_value(farmware_name='SendCmd', config_name='param1', value_type=int)
-#param2 = get_config_value(farmware_name='SendCmd', config_name='param2', value_type=int)
+param1 = get_config_value(farmware_name='SendCmd', config_name='param1', value_type=int)
+param2 = get_config_value(farmware_name='SendCmd', config_name='param2', value_type=int)
 payload = {"DeviceName":deviceID, "CmdName":cmd}
-device.log('Creatred the payload')
 
 resp = requests.post('https://hydrobotapi.azurewebsites.net/api/Hydrobot', json=payload, headers=headers)
 print(resp.text) 
-device.log('Finising SendCmd')
