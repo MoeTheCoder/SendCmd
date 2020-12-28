ddd08d983a5cbbab20852ffe5ab61c069ceec20d
MoeTheCoder <41845219+MoeTheCoder@users.noreply.github.com>
2020-12-27 18:31:21 -0700

    Update SendCmd.py

--- SendCmd.py
+++ SendCmd.py
@@ -1,15 +1,19 @@
 import requests
 
-# Farmware
-from farmware_tools import get_config_value
+try:
+  # Farmware
+  from farmware_tools import get_config_value
 
-# Declare vars
-headers = {'Content-Type': 'application/json'}
-deviceID = get_config_value(farmware_name='SendCmd', config_name='deviceID', value_type=str)
-cmd = get_config_value(farmware_name='SendCmd', config_name='cmd', value_type=str)
-param1 = get_config_value(farmware_name='SendCmd', config_name='param1', value_type=int)
-param2 = get_config_value(farmware_name='SendCmd', config_name='param2', value_type=int)
-payload = {"DeviceName":deviceID, "CmdName":cmd, "Parameter1": param1, "Parameter2": param2}
+  # Declare vars
+  headers = {'Content-Type': 'application/json'}
+  deviceID = get_config_value(farmware_name='SendCmd', config_name='deviceID', value_type=str)
+  cmd = get_config_value(farmware_name='SendCmd', config_name='cmd', value_type=str)
+  param1 = get_config_value(farmware_name='SendCmd', config_name='param1', value_type=int)
+  param2 = get_config_value(farmware_name='SendCmd', config_name='param2', value_type=int)
+  payload = {"DeviceName":deviceID, "CmdName":cmd, "Parameter1": param1, "Parameter2": param2}
 
-resp = requests.post('https://hydrobotapi.azurewebsites.net/api/Hydrobot', json=payload, headers=headers)
-print(resp.text)
+  resp = requests.post('https://hydrobotapi.azurewebsites.net/api/Hydrobot', json=payload, headers=headers)
+  print(resp.text)
+
+except Exception as error:
+    farmware_tools.device.log(repr(error))
