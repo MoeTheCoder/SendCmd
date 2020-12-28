0325ee7001c3102b56d37630b54df3e079fedd8a
MoeTheCoder <41845219+MoeTheCoder@users.noreply.github.com>
2020-12-27 18:56:07 -0700

    Update SendCmd.py

--- SendCmd.py
+++ SendCmd.py
@@ -4,11 +4,11 @@ import requests
 from farmware_tools import get_config_value
 from farmware_tools import device
 
-device.log('Starting SendCmd')
-
 # Declare vars
 headers = {'Content-Type': 'application/json'}
+device.log('Got the header')
 deviceID = get_config_value(farmware_name='SendCmd', config_name='deviceID', value_type=str)
+device.log('Got the device ID')
 cmd = get_config_value(farmware_name='SendCmd', config_name='cmd', value_type=str)
 #param1 = get_config_value(farmware_name='SendCmd', config_name='param1', value_type=int)
 #param2 = get_config_value(farmware_name='SendCmd', config_name='param2', value_type=int)
